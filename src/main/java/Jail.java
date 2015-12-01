27376837e5922ea52cc940f2623549f6f2116dc7
michaelchiucw <michaelchiucw@gmail.com>
2015-12-01 11:32:58 +0000

    Updated Jail class, have not finished yet.

--- src/main/java/Jail.java
+++ src/main/java/Jail.java
@@ -1,5 +1,20 @@
 public class Jail extends Square {
-
+  
+  // Data members
+  private int numOfPlayerInJail;
+  // Need a map to remember number of rounds left for each player in jail.
+  
+  /**
+   * Jail Constructor
+   */
+  public Jail() {
+    this.numOfPlayerInJail = 0;
+  }
+  
+  public void goJail() {
+    this.numOfPlayerInJail++;
+  }
+  
   public boolean did_Pay() {
     return true;
   }
